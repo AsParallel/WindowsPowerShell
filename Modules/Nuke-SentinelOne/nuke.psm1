a9142159ff2513ab813cfd4eec2abea8014e32a9
Quinn <QuinnK@mkcorp.com>
2016-07-28 11:57:52 -0500

    Adding nuke sentinel one

---
+++ Modules/Nuke-SentinelOne/nuke.psm1
@@ -0,0 +1,20 @@
+
+n Nuke(){
+  [cmdletbinding()]
+  $sentinelOne = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SentinelAgent";
+  $error = $false;
+  Try{
+    New-Itemproperty -Path HKLM:\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SentinelAgent -Name Start -Value 4 -PropertyType DWORD -force | out-null
+  }
+  catch{
+    $error = $true;
+  }
+  finally{
+    if($error){
+      "An error occured nuking sentinel one"
+    }
+    else{ "Sentinel One Nuked, Please Restart"; }
+  }
+}
+
+
